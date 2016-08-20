22d182b7d7910ecc20ed6e4e31108b42d0e8c2ff
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-20 06:54:37 -0500

    creando modelo user y haciendo migrate del mismo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820112831) do
+ActiveRecord::Schema.define(version: 20160820115358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20160820112831) do
     t.datetime "updated_at", null: false
     t.integer  "list_id"
     t.index ["list_id"], name: "index_tasks_on_list_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "lastname"
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "tasks", "lists"
