787cbd78ea2cf8d7c227960bcad700e51dd32be3
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-20 06:31:10 -0500

    generando FK entre list y task, has_many y belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160820112831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "lists", force: :cascade do |t|
+    t.string   "task"
+    t.string   "title"
+    t.string   "note"
+    t.boolean  "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "note"
+    t.boolean  "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "list_id"
+    t.index ["list_id"], name: "index_tasks_on_list_id", using: :btree
+  end
+
+  add_foreign_key "tasks", "lists"
 end
