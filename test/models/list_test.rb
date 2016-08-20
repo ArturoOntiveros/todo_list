2b12f52f0fc7b8220998dc68e60cb3a302942a4d
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-20 06:41:35 -0500

    poniendo test en task y list

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -4,4 +4,31 @@ class ListTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+	@ptr=lists(:one)
+  end
+
+  test "campo task no null"
+    assert_not_nil(@ptr.task,'task null')
+  end
+
+  test "campo task no vacio"
+	assert_not_empty(@ptr.task, 'list without task')
+  end
+
+  test "campo title no null"
+    assert_not_nil(@ptr.title,'title null')
+  end
+
+  test "campo title no vacio"
+	assert_not_empty(@ptr.title, 'task without title')
+  end
+
+  test "campo completed no null"
+    assert_not_nil(@ptr.completed,'completed null')
+  end
+
+  test "campo completed no vacio"
+	assert_not_empty(@ptr.title, 'task without completed')
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,23 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+	@ptr=tasks(:one)
+  end
+
+  test "campo title no null"
+    assert_not_nil(@ptr.title,'title null')
+  end
+
+  test "campo title no vacio"
+	assert_not_empty(@ptr.title, 'task without title')
+  end
+
+  test "campo completed no null"
+    assert_not_nil(@ptr.completed,'completed null')
+  end
+
+  test "campo completed no vacio"
+	assert_not_empty(@ptr.title, 'task without completed')
+  end
 end
