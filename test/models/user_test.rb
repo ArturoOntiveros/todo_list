546836ac75b797f2f0c21c27cd52be4637d46735
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-20 07:06:23 -0500

    test para campos obligatorios y unicos

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,38 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+	@ptr=users(:one)
+	@ptr2=users(:two)
+  end
+
+  test "Nombre obligatorio"
+  	assert_empty(@ptr.name, 'Sin nombre')
+  	assert_empty(@ptr2.name, 'Sin nombre')  	
+  end
+
+  test "Apellido obligatorio"
+  	assert_empty(@ptr.lastname, 'Sin Apellido')
+  	assert_empty(@ptr2.lastname, 'Sin Apellido')  	
+  end  
+
+  test "Nombre de usuario obligatorio"
+  	assert_empty(@ptr.username, 'Sin nombre de usuario')
+  	assert_empty(@ptr2.username, 'Sin nombre de usuario')  	
+  end
+
+  test "Nombre repetido"
+  	assert_same(@ptr.username,@ptr2.username,'Usuario ya existe')
+  end
+
+  test "email obligatorio"
+  	assert_empty(@ptr.email, 'Sin email')
+  	assert_empty(@ptr2.email, 'Sin email')  	
+  end  
+
+  test "Email repetido"
+  	assert_same(@ptr.email,@ptr2.email,'Email ya registrado')
+  end
+
 end
